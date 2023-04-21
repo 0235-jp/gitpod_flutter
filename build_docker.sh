d334a48802c37d002b902915071d61feb427ddae
KoheiYamashita <k-yamashita@kitproject.info>
2023-04-21 14:14:53 +0000

    Fix flutter default version

--- build_docker.sh
+++ build_docker.sh
@@ -12,7 +12,6 @@ docker build --tag kitproject/gitpod-flutter-base:lastest \
 docker build --tag kitproject/gitpod-flutter-android:lastest \
              --tag kitproject/gitpod-flutter-android:v${FLUTTER_VERSION/+/-} \
              --build-arg ANDROID_SDK_VERSION=$ANDROID_SDK_VERSION \
-             --build-arg ANDROID_NDK_VERSION=$ANDROID_NDK_VERSION \
              --build-arg BUILD_TOOLS_VERSION=$BUILD_TOOLS_VERSION \
              --build-arg PLATFORMS_VERSION=$PLATFORMS_VERSION \
              --build-arg SOURCES_VERSION=$SOURCES_VERSION \
