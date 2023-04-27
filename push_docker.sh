e9e0014084a318afaa70f634581f79a708a60b46
KoheiYamashita <k-yamashita@kitproject.info>
2023-04-27 09:34:55 +0000

    Revert "Change use github packages"
    This reverts commit 4106a2a0ecacf01c88ac0f67cdcb47e2ada27d6a.

--- push_docker.sh
+++ push_docker.sh
@@ -3,9 +3,9 @@ cd `dirname $0`
 
 source version
 
-echo $TOKEN | docker login ghcr.io -u $USER_NAME --password-stdin
+echo $DOCKER_PASSWORD | docker login --username $DOCKER_USER_NAME --password-stdin
 
-docker push ghcr.io/kitproject-dev/gitpod-flutter-base:lastest
-docker push ghcr.io/kitproject-dev/gitpod-flutter-base:v${FLUTTER_VERSION/+/-}
-docker push ghcr.io/kitproject-dev/gitpod-flutter-android:lastest
-docker push ghcr.io/kitproject-dev/gitpod-flutter-android:v${FLUTTER_VERSION/+/-}
+docker push kitproject/gitpod-flutter-base:lastest
+docker push kitproject/gitpod-flutter-base:v${FLUTTER_VERSION/+/-}
+docker push kitproject/gitpod-flutter-android:lastest
+docker push kitproject/gitpod-flutter-android:v${FLUTTER_VERSION/+/-}
