5646bd6fd3e675b90f02f2edd7c8f11ec66953f0
KoheiYamashita <k-yamashita@kitproject.info>
2023-04-27 10:36:33 +0000

    Add beta channel

--- build_docker.sh
+++ build_docker.sh
@@ -4,16 +4,35 @@ cd `dirname $0`
 source version
 
 docker build --tag kitproject/gitpod-flutter-base:lastest \
-             --tag kitproject/gitpod-flutter-base:v${FLUTTER_VERSION/+/-} \
-             --build-arg FLUTTER_VERSION=$FLUTTER_VERSION \
+             --tag kitproject/gitpod-flutter-base:stable \
+             --tag kitproject/gitpod-flutter-base:v${FLUTTER_STABLE_VERSION/+/-} \
+             --build-arg FLUTTER_CHANNEL=stable \
+             --build-arg FLUTTER_VERSION=$FLUTTER_STABLE_VERSION \
+             --no-cache \
+             --file Dockerfile_base .
+
+docker build --tag kitproject/gitpod-flutter-base:beta \
+             --tag kitproject/gitpod-flutter-base:v${FLUTTER_BETA_VERSION/+/-} \
+             --build-arg FLUTTER_CHANNEL=beta \
+             --build-arg FLUTTER_VERSION=$FLUTTER_BETA_VERSION \
              --no-cache \
              --file Dockerfile_base .
 
 docker build --tag kitproject/gitpod-flutter-android:lastest \
-             --tag kitproject/gitpod-flutter-android:v${FLUTTER_VERSION/+/-} \
+             --tag kitproject/gitpod-flutter-android:stable \
+             --tag kitproject/gitpod-flutter-android:v${FLUTTER_STABLE_VERSION/+/-} \
              --build-arg ANDROID_SDK_VERSION=$ANDROID_SDK_VERSION \
              --build-arg BUILD_TOOLS_VERSION=$BUILD_TOOLS_VERSION \
              --build-arg PLATFORMS_VERSION=$PLATFORMS_VERSION \
              --build-arg SOURCES_VERSION=$SOURCES_VERSION \
              --no-cache \
-             --file Dockerfile_android .
+             --file Dockerfile_android_stable .
+
+docker build --tag kitproject/gitpod-flutter-android:beta \
+             --tag kitproject/gitpod-flutter-android:v${FLUTTER_BETA_VERSION/+/-} \
+             --build-arg ANDROID_SDK_VERSION=$ANDROID_SDK_VERSION \
+             --build-arg BUILD_TOOLS_VERSION=$BUILD_TOOLS_VERSION \
+             --build-arg PLATFORMS_VERSION=$PLATFORMS_VERSION \
+             --build-arg SOURCES_VERSION=$SOURCES_VERSION \
+             --no-cache \
+             --file Dockerfile_android_beta .
--- push_docker.sh
+++ push_docker.sh
@@ -6,6 +6,12 @@ source version
 echo $DOCKER_PASSWORD | docker login --username $DOCKER_USER_NAME --password-stdin
 
 docker push kitproject/gitpod-flutter-base:lastest
-docker push kitproject/gitpod-flutter-base:v${FLUTTER_VERSION/+/-}
+docker push kitproject/gitpod-flutter-base:stable
+docker push kitproject/gitpod-flutter-base:beta
+docker push kitproject/gitpod-flutter-base:v${FLUTTER_STABLE_VERSION/+/-}
+docker push kitproject/gitpod-flutter-base:v${FLUTTER_BETA_VERSION/+/-}
 docker push kitproject/gitpod-flutter-android:lastest
-docker push kitproject/gitpod-flutter-android:v${FLUTTER_VERSION/+/-}
+docker push kitproject/gitpod-flutter-android:stable
+docker push kitproject/gitpod-flutter-android:beta
+docker push kitproject/gitpod-flutter-android:v${FLUTTER_STABLE_VERSION/+/-}
+docker push kitproject/gitpod-flutter-android:v${FLUTTER_BETA_VERSION/+/-}
